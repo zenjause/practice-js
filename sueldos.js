f9e0c127496e7d2fda841101eaf85661a5f3ffd5
zenjause <jesus.nf22@gmail.com>
2022-05-02 19:51:10 -0500

    analisis de sueldos

---
+++ sueldos.js
@@ -0,0 +1,49 @@
+const colombia = [];
+colombia.push({
+    name :"Juanita",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Natalia",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Juan",
+    salary: 1500,
+});
+
+colombia.push({
+    name: "Jose",
+    salary: 1500,
+});
+
+colombia.push({
+    name: "Jorge",
+    salary: 2000,
+});
+colombia.push({
+    name: "Jessy",
+    salary: 2500,
+});
+colombia.push({
+    name: "Janet",
+    salary: 2500,
+});
+colombia.push({
+    name: "Celia",
+    salary: 3500,
+});
+colombia.push({
+    name: "Diago",
+    salary: 3500,
+});
+colombia.push({
+    name: "Victor",
+    salary: 4000,
+});
+colombia.push({
+    name: "Elizabeth",
+    salary: 3000,
+});
